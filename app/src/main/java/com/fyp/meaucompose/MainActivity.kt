d99883cc43441ce7f17130a09f2ead67bdfca988
dhanpraja231 <dhanpraja231@gmail.com>
2023-02-14 10:02:53 +0400

    Google auth + NAVIGATION

--- app/src/main/java/com/fyp/meaucompose/MainActivity.kt
+++ app/src/main/java/com/fyp/meaucompose/MainActivity.kt
@@ -9,25 +9,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.fyp.meaucompose.ui.theme.MeauComposeTheme
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -49,7 +31,7 @@ class MainActivity : ComponentActivity() {
         // Check if user is signed in (non-null) and update UI accordingly.
         val account = GoogleSignIn.getLastSignedInAccount(this);
         if (account != null) {
-            System.out.println("launching intent to next page");
+//            System.out.println("launching intent to next page");
 //            Intent intent = new Intent(LoginActivity.this, UserHomeActivity.class);
 //            startActivity(intent);
             account.getEmail()?.let { Log.d("Signed In: ", it) };
@@ -59,8 +41,6 @@ class MainActivity : ComponentActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
 
         super.onCreate(savedInstanceState)
 
@@ -73,7 +53,8 @@ class MainActivity : ComponentActivity() {
         ) // since googleApiClient is deprecated, have to use workaround
 
 
-        signInLauncher = registerForActivityResult(StartActivityForResult(),
+        signInLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(),
             ActivityResultCallback<ActivityResult> { result ->
                 val task: Task<GoogleSignInAccount> =
                     GoogleSignIn.getSignedInAccountFromIntent(result.getData())
@@ -81,76 +62,75 @@ class MainActivity : ComponentActivity() {
             })
 
         setContent {
-
-            var username by remember{mutableStateOf<String>("")}
-
-            MeauComposeTheme {
-
-
-
-                // A surface container using the 'background' color from the theme
-                    Column(modifier = Modifier.fillMaxSize()
-                        , horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center
-                    ) {
-                        val context = LocalContext.current
-
-                        Button(
-                            onClick = {
-                                //startForResult.launch(googleSignInClient?.signInIntent)
-                                      signIn()
-                                Toast.makeText(context, "Signed in successfully", Toast.LENGTH_SHORT)
-                                    .show()
-
-                                val account = GoogleSignIn.getLastSignedInAccount(context);
-                                if (account != null) username = GoogleSignIn.getLastSignedInAccount(context)?.displayName.toString() else username = "not found"
-
-
-                            },
-
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(horizontal = 16.dp, vertical = 10.dp),
-                                //.height(100.dp)
-                                //.padding(start = 16.dp, end = 16.dp),
-                            shape = RoundedCornerShape(6.dp),
-                            colors = ButtonDefaults.buttonColors(
-                                backgroundColor = Color.Black,
-                                contentColor = Color.White
-                            )
-                        ) {
-                            Image(
-                                painter = painterResource(id = R.drawable.icons8_google),
-                                contentDescription = ""
-                            )
-                            Text(text = "Sign in with Google", modifier = Modifier.padding(6.dp))
-                        }
-                        Button(
-                            onClick = {
-                                //startForResult.launch(googleSignInClient?.signInIntent)
-                                signOut()
-                                username = "anonymous"
-                            },
-
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                //.height(100.dp)
-                                .padding(start = 16.dp, end = 16.dp),
-                            shape = RoundedCornerShape(6.dp),
-                            colors = ButtonDefaults.buttonColors(
-                                backgroundColor = Color.Black,
-                                contentColor = Color.White
-                            )
-                        ) {
-                            Image(
-                                painter = painterResource(id = R.drawable.icons8_google),
-                                contentDescription = ""
-                            )
-                            Text(text = "Sign out with Google", modifier = Modifier.padding(6.dp))
-                        }
-                        Text(text = "$username", fontSize = 30.sp, textAlign = TextAlign.Center)
-                    }
-            }
+            LoginScreen(mGoogleSignInClient,signInLauncher)
         }
+
+//            var username by remember{mutableStateOf<String>("")}
+//
+//            MeauComposeTheme {
+//
+//                Column(modifier = Modifier.fillMaxSize()
+//                    , horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center
+//                ) {
+//                    val context = LocalContext.current
+//
+//                    Button(
+//                        onClick = {
+//                            //startForResult.launch(googleSignInClient?.signInIntent)
+//                                  signIn()
+//                            Toast.makeText(context, "Signed in successfully", Toast.LENGTH_SHORT)
+//                                .show()
+//
+//                            val account = GoogleSignIn.getLastSignedInAccount(context);
+//                            if (account != null) username = GoogleSignIn.getLastSignedInAccount(context)?.displayName.toString() else username = "not found"
+//
+//
+//                        },
+//
+//                        modifier = Modifier
+//                            .fillMaxWidth()
+//                            .padding(horizontal = 16.dp, vertical = 10.dp),
+//                            //.height(100.dp)
+//                            //.padding(start = 16.dp, end = 16.dp),
+//                        shape = RoundedCornerShape(6.dp),
+//                        colors = ButtonDefaults.buttonColors(
+//                            backgroundColor = Color.Black,
+//                            contentColor = Color.White
+//                        )
+//                    ) {
+//                        Image(
+//                            painter = painterResource(id = R.drawable.icons8_google),
+//                            contentDescription = ""
+//                        )
+//                        Text(text = "Sign in with Google", modifier = Modifier.padding(6.dp))
+//                    }
+//                    Button(
+//                        onClick = {
+//                            //startForResult.launch(googleSignInClient?.signInIntent)
+//                            signOut()
+//                            username = "anonymous"
+//                        },
+//
+//                        modifier = Modifier
+//                            .fillMaxWidth()
+//                            //.height(100.dp)
+//                            .padding(start = 16.dp, end = 16.dp),
+//                        shape = RoundedCornerShape(6.dp),
+//                        colors = ButtonDefaults.buttonColors(
+//                            backgroundColor = Color.Black,
+//                            contentColor = Color.White
+//                        )
+//                    ) {
+//                        Image(
+//                            painter = painterResource(id = R.drawable.icons8_google),
+//                            contentDescription = ""
+//                        )
+//                        Text(text = "Sign out with Google", modifier = Modifier.padding(6.dp))
+//                    }
+//                    Text(text = "$username", fontSize = 30.sp, textAlign = TextAlign.Center)
+//                }
+//            }
+//        }
     }
 
     private fun signIn() {
@@ -185,16 +165,16 @@ class MainActivity : ComponentActivity() {
 
 
 
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MeauComposeTheme {
-        Greeting("Android")
-    }
-}
+//
+//@Composable
+//fun Greeting(name: String) {
+//    Text(text = "Hello $name!")
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    MeauComposeTheme {
+//        Greeting("Android")
+//    }
+//}
